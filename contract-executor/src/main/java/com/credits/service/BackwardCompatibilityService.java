a2ef0fd5831c01e4748e37c1ac6965fe42927b14
Igor Goryunov <kongostream@gmail.com>
2019-12-08 10:47:25 +0300

    fix: add smart-contract.v3 to initialize

--- contract-executor/src/main/java/com/credits/service/BackwardCompatibilityService.java
+++ contract-executor/src/main/java/com/credits/service/BackwardCompatibilityService.java
@@ -6,7 +6,8 @@ public class BackwardCompatibilityService {
 
     public static final List<Class<?>> allVersionsSmartContractClass = List.of(com.credits.scapi.v0.SmartContract.class,
                                                                                com.credits.scapi.v1.SmartContract.class,
-                                                                               com.credits.scapi.v2.SmartContract.class);
+                                                                               com.credits.scapi.v2.SmartContract.class,
+                                                                               com.credits.scapi.v3.SmartContract.class);
     public static final List<Class<?>> allVersionsBasicStandardClass = List.of(com.credits.scapi.v0.BasicStandard.class,
                                                                                com.credits.scapi.v1.BasicTokenStandard.class);
 
